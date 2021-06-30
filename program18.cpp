42f35b6655c413748657045cca65721ae9c80e67
Dustin McPhate <dustin_mcphate@yahoo.com>
2021-06-30 16:49:07 -0500

    Add files via upload

---
+++ program18.cpp
@@ -0,0 +1,20 @@
+//CSC 1060 Spring 2011
+//Program # 18 Pg 155
+//Dustin McPhate
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+const double pi = 3.14, areaSlice = 14.125;
+int main ()
+{
+double diameter, radius, area, slices;
+cout <<"Enter the diameter of the pizza.";
+cin >> diameter;
+radius = diameter / 2;
+area = pi * radius * radius;
+slices = area / areaSlice;
+cout  << "The number of slices of pizza will be " << slices << endl;
+return 0;
+}  
+
