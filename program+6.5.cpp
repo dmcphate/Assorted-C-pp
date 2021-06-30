e0619e11ee2723e9bba13d444e30e1009a840bad
Dustin McPhate <dustin_mcphate@yahoo.com>
2021-06-30 16:51:07 -0500

    Add files via upload

---
+++ program+6.5.cpp
@@ -0,0 +1,31 @@
+//Dustin McPhate
+//Comp 1060 program 6.5
+
+#include <iostream>
+using namespace std;
+
+double fallingDistance(double);
+
+int main()
+
+{
+	double distance, sec;
+	for (sec = 1; sec <= 10; sec ++)
+	{
+		distance = fallingDistance(sec);
+		cout << "The distance at " << sec << " seconds is " << distance  << endl;
+	}
+    
+return 0;
+}
+
+
+double fallingDistance (double sec)
+{
+	double meters, time, graV, grav = 9.8;
+	graV = (.5 * grav);
+	time = sec * sec;
+    meters = (graV * time);
+	return meters;
+}
+
